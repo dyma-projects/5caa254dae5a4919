a3f27cc26d58f56940dc3a590e7e22992d123f72
sandrine <sandrinezocca@gmail.com>
2020-02-27 16:29:40 +0100

    three binding examples

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1Component implements OnInit {
 
-  constructor() { }
+  imageLink: string;
+  luciferImage: string = 'https://avatarfiles.alphacoders.com/184/thumb-184206.jpg';
+  author: string = 'Edsger Dijkstra = la variable interpolée'
+  
+  constructor() { 
+    this.imageLink = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDQZQjH3A3tI1vrmaThKgoRc3Wx0BTSFLtVhpNNW_aTwkoNfIm';
+    this.luciferImage
+  }
+  
+  switchImageEnter(event: Event): void{
+    console.log(event);
+    this.luciferImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQirIDlO8V4z-CxQPpq44mLyEDel8ae101qarxfk4ZOrpeHA9PF';
+  }
+
+  switchImageLeave(event: Event): void{
+    console.log(event);
+    this.luciferImage = 'https://avatarfiles.alphacoders.com/184/thumb-184206.jpg';
+  }
 
   ngOnInit() {
   }
